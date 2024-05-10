9256d0e68dcfab6f67004615f7948f4bf968106f
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-05-10 10:33:48 +1000

    Dev (#7)

--- aws_s3_object.tf
+++ aws_s3_object.tf
@@ -15,3 +15,21 @@ resource "aws_s3_object" "logs" {
   force_destroy    = true
   key              = "logs/"
 }
+
+resource "aws_s3_object" "rekognition" {
+  acl              = "private"
+  bucket           = aws_s3_bucket.main.bucket
+  content_language = "en-US"
+  content_type     = "application/x-directory"
+  force_destroy    = true
+  key              = "rekognition/"
+}
+
+resource "aws_s3_object" "s3_bucket_notification" {
+  acl              = "private"
+  bucket           = aws_s3_bucket.main.bucket
+  content_language = "en-US"
+  content_type     = "application/x-directory"
+  force_destroy    = true
+  key              = "s3_bucket_notification/"
+}
