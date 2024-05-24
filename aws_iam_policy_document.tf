762948622ac2cb9122f5331be43d68a892723430
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-05-24 15:30:35 +1000

    Dev (#18)

--- aws_iam_policy_document.tf
+++ aws_iam_policy_document.tf
@@ -24,6 +24,19 @@ data "aws_iam_policy_document" "assume_role_sfn_state_machine_rekognition_detect
   }
 }
 
+data "aws_iam_policy_document" "assume_role_sfn_state_machine_rekognition_detect_moderation_labels" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    effect = "Allow"
+    principals {
+      identifiers = ["states.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
 data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_faces" {
   statement {
     actions = [
@@ -50,16 +63,6 @@ data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_faces" {
 
   statement {
     actions = [
-      "s3:PutObject"
-    ]
-    effect = "Allow"
-    resources = [
-      "${aws_s3_bucket.main.arn}/rekognition/detect_faces/*"
-    ]
-  }
-
-  statement {
-    actions = [
       "rekognition:DetectFaces"
     ]
     effect = "Allow"
@@ -73,15 +76,8 @@ data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_labels" {
   statement {
     actions = [
       "s3:GetObject",
-      "s3:GetObjectAcl",
       "s3:GetObjectAttributes",
-      "s3:GetObjectTagging",
-      "s3:GetObjectVersion",
-      "s3:GetObjectVersionAcl",
-      "s3:GetObjectVersionAttributes",
-      "s3:GetObjectVersionForReplication",
-      "s3:GetObjectVersionTagging",
-      "s3:GetObjectVersionTorrent",
+      "s3:GetObjectVersion"
     ]
     effect = "Allow"
     resources = [
@@ -91,7 +87,53 @@ data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_labels" {
 
   statement {
     actions = [
+      "s3:GetBucketVersioning",
+      "s3:ListBucket"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.main.arn}"
+    ]
+  }
+
+  statement {
+    actions = [
       "rekognition:DetectLabels"
+    ]
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_moderation_labels" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAttributes",
+      "s3:GetObjectVersion"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.main.arn}/${aws_s3_object.images.key}*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetBucketVersioning",
+      "s3:ListBucket"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.main.arn}"
+    ]
+  }
+
+  statement {
+    actions = [
+      "rekognition:DetectModerationLabels"
     ]
     effect = "Allow"
     resources = [
