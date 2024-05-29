43b1f523adeb54dda31a21da3d898f7651a7578f
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-05-29 23:14:37 +0000

    Dev

--- aws_iam_role.tf
+++ aws_iam_role.tf
@@ -57,3 +57,18 @@ resource "aws_iam_role" "sfn_state_machine_rekognition_detect_protective_equipme
     workspace                   = terraform.workspace
   }
 }
+
+resource "aws_iam_role" "sfn_state_machine_rekognition_detect_text" {
+  assume_role_policy = data.aws_iam_policy_document.assume_role_sfn_state_machine_rekognition_detect_text.json
+  name               = "sfn-state-machine-rekognition-detect-text"
+  path               = "/${local.organization}/"
+  tags = {
+    caller_identity_account_arn = data.aws_caller_identity.main.arn
+    caller_identity_account_id  = data.aws_caller_identity.main.account_id
+    caller_identity_user_id     = data.aws_caller_identity.main.user_id
+    canonical_user_id           = data.aws_canonical_user_id.main.id
+    organization                = local.organization
+    region                      = data.aws_region.main.name
+    workspace                   = terraform.workspace
+  }
+}
