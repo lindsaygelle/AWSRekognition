37148f82613628eb50ef15ccb942d610f1b1c04f
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-05-29 14:23:44 +1000

    Dev (#21)

--- aws_iam_role.tf
+++ aws_iam_role.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "sfn_state_machine_rekognition_detect_faces" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_sfn_state_machine_rekognition_detect_faces.json
-  name               = "${local.organization}-sfn-state-machine-rekognition-detect-faces"
+  name               = "sfn-state-machine-rekognition-detect-faces"
   path               = "/${local.organization}/"
   tags = {
     caller_identity_account_arn = data.aws_caller_identity.main.arn
@@ -15,7 +15,7 @@ resource "aws_iam_role" "sfn_state_machine_rekognition_detect_faces" {
 
 resource "aws_iam_role" "sfn_state_machine_rekognition_detect_labels" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_sfn_state_machine_rekognition_detect_labels.json
-  name               = "${local.organization}-sfn-state-machine-rekognition-detect-labels"
+  name               = "sfn-state-machine-rekognition-detect-labels"
   path               = "/${local.organization}/"
   tags = {
     caller_identity_account_arn = data.aws_caller_identity.main.arn
@@ -30,7 +30,22 @@ resource "aws_iam_role" "sfn_state_machine_rekognition_detect_labels" {
 
 resource "aws_iam_role" "sfn_state_machine_rekognition_detect_moderation_labels" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_sfn_state_machine_rekognition_detect_moderation_labels.json
-  name               = "${local.organization}-sfn-state-machine-rekognition-detect-moderation-labels"
+  name               = "sfn-state-machine-rekognition-detect-moderation-labels"
+  path               = "/${local.organization}/"
+  tags = {
+    caller_identity_account_arn = data.aws_caller_identity.main.arn
+    caller_identity_account_id  = data.aws_caller_identity.main.account_id
+    caller_identity_user_id     = data.aws_caller_identity.main.user_id
+    canonical_user_id           = data.aws_canonical_user_id.main.id
+    organization                = local.organization
+    region                      = data.aws_region.main.name
+    workspace                   = terraform.workspace
+  }
+}
+
+resource "aws_iam_role" "sfn_state_machine_rekognition_detect_protective_equipment" {
+  assume_role_policy = data.aws_iam_policy_document.assume_role_sfn_state_machine_rekognition_detect_protective_equipment.json
+  name               = "sfn-state-machine-rekognition-detect-protective-equipment"
   path               = "/${local.organization}/"
   tags = {
     caller_identity_account_arn = data.aws_caller_identity.main.arn
--- aws_iam_role_policy.tf
+++ aws_iam_role_policy.tf
@@ -12,3 +12,8 @@ resource "aws_iam_role_policy" "sfn_state_machine_rekognition_detect_moderation_
   policy = data.aws_iam_policy_document.sfn_state_machine_rekognition_detect_moderation_labels.json
   role   = aws_iam_role.sfn_state_machine_rekognition_detect_moderation_labels.id
 }
+
+resource "aws_iam_role_policy" "sfn_state_machine_rekognition_detect_protective_equipment" {
+  policy = data.aws_iam_policy_document.sfn_state_machine_rekognition_detect_protective_equipment.json
+  role   = aws_iam_role.sfn_state_machine_rekognition_detect_protective_equipment.id
+}
