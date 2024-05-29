21b7b549c8742539f09bb674db74effa5734ca71
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-05-29 11:46:43 +1000

    Dev (#20)

--- aws_sfn_state_machine.tf
+++ aws_sfn_state_machine.tf
@@ -33,3 +33,21 @@ resource "aws_sfn_state_machine" "rekognition_detect_labels" {
     workspace                   = terraform.workspace
   }
 }
+
+resource "aws_sfn_state_machine" "rekognition_detect_moderation_labels" {
+  definition = templatefile("./state_machine/RekognitionDetectModerationLabels.json", {})
+  name       = "${local.organization}-rekognition-detect-moderation-labels"
+  role_arn   = aws_iam_role.sfn_state_machine_rekognition_detect_moderation_labels.arn
+  tags = {
+    bucket                      = aws_s3_bucket.main.bucket
+    bucket_key                  = aws_s3_object.images.key
+    caller_identity_account_arn = data.aws_caller_identity.main.arn
+    caller_identity_account_id  = data.aws_caller_identity.main.account_id
+    caller_identity_user_id     = data.aws_caller_identity.main.user_id
+    canonical_user_id           = data.aws_canonical_user_id.main.id
+    organization                = local.organization
+    rekognition                 = "DetectModerationLabels"
+    region                      = data.aws_region.main.name
+    workspace                   = terraform.workspace
+  }
+}
