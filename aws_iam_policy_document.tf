88754dbf651bec64ea57fe43a1794817b89ad839
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-05-29 02:37:24 +0000

    Dev

--- aws_iam_policy_document.tf
+++ aws_iam_policy_document.tf
@@ -37,6 +37,19 @@ data "aws_iam_policy_document" "assume_role_sfn_state_machine_rekognition_detect
   }
 }
 
+data "aws_iam_policy_document" "assume_role_sfn_state_machine_rekognition_detect_protective_equipment" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    effect = "Allow"
+    principals {
+      identifiers = ["states.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
 data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_faces" {
   statement {
     actions = [
@@ -134,6 +147,41 @@ data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_moderation_
   statement {
     actions = [
       "rekognition:DetectModerationLabels"
+    ]
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "sfn_state_machine_rekognition_detect_protective_equipment" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAttributes",
+      "s3:GetObjectVersion"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.main.arn}/${aws_s3_object.images.key}*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetBucketVersioning",
+      "s3:ListBucket"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.main.arn}"
+    ]
+  }
+
+  statement {
+    actions = [
+      "rekognition:DetectProtectiveEquipment"
     ]
     effect = "Allow"
     resources = [
--- aws_sfn_state_machine.tf
+++ aws_sfn_state_machine.tf
@@ -1,6 +1,6 @@
 resource "aws_sfn_state_machine" "rekognition_detect_faces" {
   definition = templatefile("./state_machine/RekognitionDetectFaces.json", {})
-  name       = "${local.organization}-rekognition-detect-faces"
+  name       = "rekognition-detect-faces"
   role_arn   = aws_iam_role.sfn_state_machine_rekognition_detect_faces.arn
   tags = {
     bucket                      = aws_s3_bucket.main.bucket
@@ -18,7 +18,7 @@ resource "aws_sfn_state_machine" "rekognition_detect_faces" {
 
 resource "aws_sfn_state_machine" "rekognition_detect_labels" {
   definition = templatefile("./state_machine/RekognitionDetectLabels.json", {})
-  name       = "${local.organization}-rekognition-detect-labels"
+  name       = "rekognition-detect-labels"
   role_arn   = aws_iam_role.sfn_state_machine_rekognition_detect_labels.arn
   tags = {
     bucket                      = aws_s3_bucket.main.bucket
@@ -36,7 +36,7 @@ resource "aws_sfn_state_machine" "rekognition_detect_labels" {
 
 resource "aws_sfn_state_machine" "rekognition_detect_moderation_labels" {
   definition = templatefile("./state_machine/RekognitionDetectModerationLabels.json", {})
-  name       = "${local.organization}-rekognition-detect-moderation-labels"
+  name       = "rekognition-detect-moderation-labels"
   role_arn   = aws_iam_role.sfn_state_machine_rekognition_detect_moderation_labels.arn
   tags = {
     bucket                      = aws_s3_bucket.main.bucket
@@ -47,6 +47,24 @@ resource "aws_sfn_state_machine" "rekognition_detect_moderation_labels" {
     canonical_user_id           = data.aws_canonical_user_id.main.id
     organization                = local.organization
     rekognition                 = "DetectModerationLabels"
+    region                      = data.aws_region.main.name
+    workspace                   = terraform.workspace
+  }
+}
+
+resource "aws_sfn_state_machine" "rekognition_detect_protective_equipment" {
+  definition = templatefile("./state_machine/RekognitionDetectProtectiveEquipment.json", {})
+  name       = "rekognition-detect-protective-equipment"
+  role_arn   = aws_iam_role.sfn_state_machine_rekognition_detect_protective_equipment.arn
+  tags = {
+    bucket                      = aws_s3_bucket.main.bucket
+    bucket_key                  = aws_s3_object.images.key
+    caller_identity_account_arn = data.aws_caller_identity.main.arn
+    caller_identity_account_id  = data.aws_caller_identity.main.account_id
+    caller_identity_user_id     = data.aws_caller_identity.main.user_id
+    canonical_user_id           = data.aws_canonical_user_id.main.id
+    organization                = local.organization
+    rekognition                 = "DetectProtectiveEquipment"
     region                      = data.aws_region.main.name
     workspace                   = terraform.workspace
   }
